70633ec1df1a94e9735d7db009c0d27c6065813c
irsisyphus <cgao96@gmail.com>
2017-03-17 12:34:40 -0700

    alpha

--- main.py
+++ main.py
@@ -177,8 +177,8 @@ def test():
     -------------------------------
     """
     numTest = 20       # number of tests for each configuration
-    difficulty = 'E'  # E - easy, M - medium, H - high
-    timeout = 30     # set timeout to exit, will display 'infs'
+    difficulty = 'E'   # E - easy, M - medium, H - high
+    timeout = 30       # set timeout to exit, will display 'infs'
 
     consisList = ['ForwardChecking', 'ArcConsistency', 'NKD', 'NKT']
     outfile = open('log/P' + difficulty + '.txt', 'w+')
